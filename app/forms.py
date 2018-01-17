273ba5acc151a721cb33980bc85dfdb666cd78d2
keer2345 <keer2345@gmail.com>
2018-01-17 22:57:26 +0800

    ch06 edit_profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo,ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from app.models import User
 
 
@@ -19,12 +19,20 @@ class RegistrationForm(FlaskForm):
         'Repat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise  ValidationError('This username is exists, please use a different username.')
+            raise ValidationError(
+                'This username is exists, please use a different username.')
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise  ValidationError('This email is exists, please use a different email address.')
+            raise ValidationError(
+                'This email is exists, please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
