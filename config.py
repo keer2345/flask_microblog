8c41f9a97cc941b3b8052c4a5b363a5e3eba4e68
keer2345 <keer2345@gmail.com>
2018-01-19 10:37:16 +0800

    ch07

--- config.py
+++ config.py
@@ -10,3 +10,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.sina.com.cn'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None or 1
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'keer2345'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '912229hui'
+    ADMINS = ['keer2345@163.com']
+    # ADMINS = ['admin@example.com']
