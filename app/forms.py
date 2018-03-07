0a905381f2f57bbc31e64d899864d9daa709c44b
keer2345 <keer2345@gmail.com>
2018-03-07 14:45:28 +0800

    add post in homepage

--- app/forms.py
+++ app/forms.py
@@ -48,3 +48,9 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please user a different useranem.')
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField(
+        'Say something', validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,8 @@ from werkzeug.urls import url_parse
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 
 from . import app, db
-from app.models import User
+from app.models import User, Post
+from app.forms import PostForm
 
 
 @app.before_request
@@ -17,12 +18,20 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
-# @login_required
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [{
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
+    posts = current_user.followed_posts().all()
+    postss = [{
         'author': {
             'username': 'John'
         },
@@ -34,7 +43,8 @@ def index():
         'body': 'The Avengers movie was so cool!'
     }]
 
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template(
+        'index.html.j2', title='Home', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
